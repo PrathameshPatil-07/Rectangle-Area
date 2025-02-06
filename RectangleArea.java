6a8a1cd23237bd377c22af200475c6fa4a6c680e
PrathameshPatil-07 <prathmeshpatil495@gmail.com>
2025-02-06 19:37:36 +0530

    RectangleArea.java

---
+++ RectangleArea.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Area{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+	
+		//Rectangle
+		System.out.println("Enter length :");
+		int l=sc.nextInt();
+		System.out.println("Enter width :");
+		int w=sc.nextInt();
+		int arearect=l*w;
+
+		System.out.print("Area of Rectangle is :");
+		System.out.println(arearect);
+
+		
+		
+		}
+		}
