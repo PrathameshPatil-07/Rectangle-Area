c7712c2ca33c8502c2e2f8d94f80a14b961bf993
PrathameshPatil-07 <prathmeshpatil495@gmail.com>
2025-02-07 11:28:50 +0530

    Create Rectangle Perimeter.java

---
+++ Rectangle Perimeter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Area{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+	
+		//Rectangle
+		System.out.println("Enter length :");
+		int l=sc.nextInt();
+		System.out.println("Enter width :");
+		int w=sc.nextInt();
+		int peri=2(l+w);
+
+		System.out.print("Area of Rectangle is :" + peri);
+	
+
+		
+		
+		}
+		}
